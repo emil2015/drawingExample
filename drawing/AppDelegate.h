2cb575edaac923178e446ba0836db87bbf2d96aa
David Gross <emil2015@gmail.com>
2014-10-29 16:06:40 -0400

    Initial Commit

---
+++ drawing/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  drawing
+//
+//  Created by David Gross on 10/29/14.
+//  Copyright (c) 2014 GrossProfitEnterprises. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ drawing/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  drawing
+//
+//  Created by David Gross on 10/29/14.
+//  Copyright (c) 2014 GrossProfitEnterprises. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
